9e706405e08bb5cd0d48fadaa2bdc540ca3052e5
K-selenga <koc.selenga@gmail.com>
2023-02-05 01:44:31 -0800

    Form structure implemented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./style/main.css";
 import Login from "./Login";
 import Register from "./Register";
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>...Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        placeholder="Password"
+        required
+      />
+      <button type="submit">Sign In</button>
+    </form>
+  );
 };
 
 export default Login;
