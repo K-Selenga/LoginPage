d0f32c5db2e3daa850dd977c14f52d77a89a05bf
K-selenga <koc.selenga@gmail.com>
2023-02-11 20:53:43 -0800

    checkbox added

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,36 +13,52 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-        placeholder="Password"
-        required
-      />
-      <label>
-        <input
-          type="checkbox"
-          checked={rememberMe}
-          onChange={(event) => setRememberMe(event.target.checked)}
-        />
-        Remember Me
-      </label>
-      <button type="submit">Sign In</button>
-      <div>
-        <a href="#">Forgot password?</a>
-        <a href="#">Create an account</a>
-        <a href="#">Continue as guest</a>
+    <>
+      <div className="login">
+        <div className="login__box">
+          <form onSubmit={handleSubmit} className="login__box--form">
+            <h2 className="login__box--form--title">Sign in</h2>
+            <div className="login__box--form--content">
+              <h3 className="login__box--form--content--title2">Email</h3>
+              <input
+                className="login__box--form--content--input"
+                type="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder=""
+                required
+              />
+              <h3 className="login__box--form--content--title2">Password</h3>
+              <input
+                className="login__box--form--content--input"
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                placeholder=""
+                required
+              />
+              <label className="container">
+                {" "}
+                Remember me?
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  checked={rememberMe}
+                  onChange={(event) => setRememberMe(event.target.checked)}
+                />
+                <span className="checkmark"></span>
+              </label>
+              <button type="submit">Sign In</button>
+            </div>
+            <div>
+              <a href="#">Forgot your password?</a>
+              <a href="#">Sign up</a>
+              <a href="#">Continue as guest</a>
+            </div>
+          </form>
+        </div>
       </div>
-    </form>
+    </>
   );
 };
 
